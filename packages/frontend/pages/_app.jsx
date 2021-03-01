eb95fb88d1134119f12132a46b018ecd8d7a89fc
Nikhil Mehta <nikhil.001mehta@gmail.com>
2021-03-01 15:45:03 +0530

    fix: fixed jsx prop camel case issue

--- packages/frontend/pages/_app.jsx
+++ packages/frontend/pages/_app.jsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>{Component.title}</title>
         <meta charSet="utf-8" />
-        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta property="og:type" content={Component.title} />
         <meta name="description" content={Component.title} />
