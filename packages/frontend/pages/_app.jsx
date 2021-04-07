e976fa10a6e50226db07c738ccf7143d1cb774fe
Nikhil Mehta <nikhil.001mehta@gmail.com>
2021-04-07 13:17:34 +0530

    chore: added GTM

--- packages/frontend/pages/_app.jsx
+++ packages/frontend/pages/_app.jsx
@@ -14,6 +14,15 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5LL2N68');`,
+          }}
+        ></script>
         <meta property="og:type" content="website" />
         <meta name="description" content={text.seoTagLine} />
         <meta name="og:description" content={text.seoTagLine} />
@@ -22,6 +31,14 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="@technikhil314" />
       </Head>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5LL2N68"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
       <RhinoProvider>
         <Navbar />
         <main
